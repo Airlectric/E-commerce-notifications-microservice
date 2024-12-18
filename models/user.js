02d9937e701dbd432c28ab21431732e41950ba44
Doe Agudey Daniel <agudeydaniel8@gmail.com>
2024-12-18 20:23:23 +0000

    added authentication messages

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   id: { type: String, unique: true, required: true },
   username: { type: String, required: true },
   email: { type: String, required: true },
-  email: { type: String, required: true },
+  role: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
